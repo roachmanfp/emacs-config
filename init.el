24b4cfc96b934ad786e0ae16357d146ff23e0f1d
Ross Roach <roachmanfp@gmail.com>
2024-01-07 15:27:17 -0600

    Added mermaid diagram support

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(diff-hl corfu yaml-mode yafolding wgrep vertico use-package undo-fu-session treemacs-magit treemacs-icons-dired pdf-tools mood-line markdown-mode marginalia eglot company ample-theme)))
+   '(mermaid-mode diff-hl corfu yaml-mode yafolding wgrep vertico use-package undo-fu-session treemacs-magit treemacs-icons-dired pdf-tools mood-line markdown-mode marginalia eglot company ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
