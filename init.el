73f474d149cecfbb6d3ca8a24daee502993867ad
roachmanfp <ross.roach1@gmail.com>
2024-01-07 01:37:17 -0600

    Removed org export file. Added org export init.el

---
+++ init.el
@@ -0,0 +1,14 @@
+(org-babel-load-file "~/.config/emacs/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(diff-hl corfu yaml-mode yafolding wgrep vertico use-package undo-fu-session treemacs-magit treemacs-icons-dired pdf-tools mood-line markdown-mode marginalia eglot company ample-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
